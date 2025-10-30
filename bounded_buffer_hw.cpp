fadccd6e0734df4e4e44fda2d83917c290979608
Sana (Q) <@CocoaPuff2>
2025-10-30 13:41:32 -0700

    adding code

--- bounded_buffer_hw.cpp
+++ bounded_buffer_hw.cpp
@@ -13,23 +13,52 @@ BoundedBufferHW::BoundedBufferHW( int bufsize ) {
   sem_e.init(&empty, 0, 0); /* Initially, buf has zero data items */
 }
 
-/* Clean up buffer sp */
+/* Destructor: Clean up buffer sp */
 BoundedBufferHW::~BoundedBufferHW( ) {
+    // delete[] buf;
   delete buf;
 }
 
-/* Insert item onto the rear of shared buffer sp */
+// Insert item onto the rear of shared buffer sp, blocks w/ busy-wait if full
+/*   Pseudocode:
+ *     wait (full) - for free slot
+ *     wait (mutex) - acquire lock enter Crit Sec
+ *     Add item to buffer (cicular logic)
+ *     post (mutex) - release lock, other threads can access buffer
+ *     post (empty) - signal, new item availible for consumers
+ */
 void BoundedBufferHW::insert( int item ) {
-  /* Implement by yourself */
-  // todo
+  sem_f.wait(&full);
+  sem_m.wait(&mutex);
+
+  buf[rear % n] = item;
+  rear++; // index moved to next pos
+
+  sem_m.post(&mutex);
+  sem_e.post(&empty);
  }
 
 /* Remove and return the first item from buffer sp */
-int BoundedBufferHW::remove( )
-{
- int item;
-  /* Implement by yourself */
-  // todo
- return item;
+/*   Pseudocode:
+ *     wait (empty) - for free slot
+ *     wait (mutex) - acquire lock enter Crit Sec
+ *     Remove item from buffer (cicular logic)
+ *     post (mutex) - release lock, other threads can access buffer
+ *     post (full) - signal, new item availible for consumers
+ */
+// todo
+int BoundedBufferHW::remove( ) {
+    int item;
+
+    sem_e.wait(&empty);
+    sem_m.wait(&mutex);
+
+    item = buf[front % n];
+    front++; // moves the index to the next item in buffer
+
+    sem_m.post(&mutex);
+    sem_f.post(&full);
+
+    return item;
  }
 
